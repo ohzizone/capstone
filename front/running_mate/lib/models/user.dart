3774902c9d2733575ebdc663b71a2e98969cdb21
조하은 <108499120+hani0903@users.noreply.github.com>
2024-05-04 18:02:45 +0900

    코드 수정

---
+++ front/running_mate/lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final String userId;
+  final String password;
+
+  User({
+    required this.userId,
+    required this.password,
+  });
+}
---
+++ front/running_mate/lib/screens/dataPageState.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart'
+
+
+// import 'package:hive/hive.dart';class DataPage extends StatefulWidget {
+//   const DataPage({super.key});
+
+//   @override
+//   State<DataPage> createState() => _MyWidgetState();
+// }
+
+// class _DataPageState extends State<DataPage> {
+
+//   var mybox = Hive.box('hivebox')
+//   List mydat = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
