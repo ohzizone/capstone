549df9db67d7eff055a7ef24e77ea21c7fb6d8d1
조하은 <108499120+hani0903@users.noreply.github.com>
2024-05-09 23:21:10 +0900

    Update

--- front/running_mate/lib/main.dart
+++ front/running_mate/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:running_mate/screens/LoginScreen.dart';
 import 'package:running_mate/screens/SplashScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 // 앱의 시작점
 // runApp 함수의 괄호 안에는 위젯이 들어간다.
 void main() async {
-  await Hive.initFlutter();
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   // var box = await Hive.openBox('dataBox');
   runApp(MyApp());
 }
