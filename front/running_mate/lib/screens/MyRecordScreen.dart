bf1c59b7eb4edb00fa14eee0e8176266914a87f1
조하은 <108499120+hani0903@users.noreply.github.com>
2024-05-28 22:28:16 +0900

    Feat: 사용자 달리기 기록 보여주기
    사용자의 달리기 기록을 마커와 Card를 통해 시각적으로 보여주도록 코드를 작성하였습니다.

--- front/running_mate/lib/screens/MyRecordScreen.dart
+++ front/running_mate/lib/screens/MyRecordScreen.dart
@@ -19,30 +19,35 @@ class _MyRecordScreenState extends State<MyRecordScreen> {
 
   DateTime focusedDay = DateTime.now();
   DateTime? selectedDay;
-  // 기록 저장
+
   Map<DateTime, List<MyRecord>> records = {};
+
+  String formatDate(DateTime date) {
+    // DateTime 객체를 원하는 형식의 문자열로 변환
+    String formattedDate = '${date.month.toString().padLeft(2, '0')}/'
+        '${date.day.toString().padLeft(2, '0')}';
+    return formattedDate;
+  }
 
   Future<void> _loadRecords() async {
     User? user = FirebaseAuth.instance.currentUser;
     final FirebaseFirestore _firestore = FirebaseFirestore.instance;
     if (user != null) {
       String userId = user.uid; // 현재 사용자의 ID 가져오기
-      // 파이어베이스에서 데이터를 읽어오는 비동기 함수
       QuerySnapshot querySnapshot = await _firestore
           .collection('running record')
-          .doc(userId) // 사용자 ID를 사용하거나 원하는 사용자의 ID를 직접 입력하세요.
+          .doc(userId)
           .collection('record')
           .get();
 
-      // 기록 저장
       Map<DateTime, List<MyRecord>> loadedRecords = {};
 
-      // 가져온 각 문서를 반복하여 처리
       querySnapshot.docs.forEach((doc) {
         Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
         if (data.containsKey('date') && data['date'] != null) {
           // Timestamp를 DateTime으로 변환
           DateTime date = (data['date'] as Timestamp).toDate();
+
           // 날짜 부분만 추출하여 UTC 형식으로 변환
           DateTime dateOnly = DateTime.utc(date.year, date.month, date.day);
           MyRecord record = MyRecord(
@@ -75,7 +80,7 @@ class _MyRecordScreenState extends State<MyRecordScreen> {
   void initState() {
     super.initState();
     initializeDateFormatting('ko-KR', null); // 날짜 형식 초기화
-    _loadRecords(); // 기록 불러오기
+    _loadRecords();
   }
 
   @override
@@ -84,84 +89,141 @@ class _MyRecordScreenState extends State<MyRecordScreen> {
       appBar: AppBar(
         title: Text('내 기록'),
       ),
-      body: TableCalendar(
-        focusedDay: DateTime.now(),
-        firstDay: DateTime(2022, 1, 1),
-        lastDay: DateTime(2029, 1, 31),
-        locale: 'ko-KR',
-        daysOfWeekHeight: 30,
-        calendarFormat: _calendarFormat,
-        selectedDayPredicate: (day) {
-          // 선택된 날짜가 현재의 _selectedDay와 같은지 비교합니다.
-          return isSameDay(selectedDay, day);
-        },
-        onFormatChanged: (format) {
-          setState(() {
-            _calendarFormat = format;
-          });
-        },
-        onDaySelected: (_selectedDay, _focusedDay) {
-          setState(() {
-            selectedDay = _selectedDay;
-            focusedDay = _focusedDay;
-          });
-        },
-        availableCalendarFormats: const {
-          CalendarFormat.month: 'Month',
-          CalendarFormat.twoWeeks: '2 Weeks',
-          CalendarFormat.week: 'Week',
-        },
-        headerStyle: HeaderStyle(
-          titleCentered: true,
-          titleTextStyle: TextStyle(
-            color: gray4,
-            fontFamily: 'PretandardMedium',
-            fontSize: 20.0,
-          ),
-          headerPadding: EdgeInsets.fromLTRB(0, 8.0, 0, 8.0),
-        ),
-        calendarStyle: CalendarStyle(
-          holidayTextStyle: TextStyle(
-            color: gray4,
-            fontFamily: 'PretandardMedium',
-            fontSize: 15.0,
-          ),
-          defaultTextStyle: TextStyle(
-            color: gray4,
-            fontFamily: 'PretandardMedium',
-            fontSize: 15.0,
-          ),
-          // today 표시 여부
-          isTodayHighlighted: true,
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: DateTime.now(),
+            firstDay: DateTime(2022, 1, 1),
+            lastDay: DateTime(2029, 1, 31),
+            locale: 'ko-KR',
+            daysOfWeekHeight: 30,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(selectedDay, day);
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            onDaySelected: (_selectedDay, _focusedDay) {
+              setState(() {
+                selectedDay = _selectedDay;
+                focusedDay = _focusedDay;
+              });
+            },
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+              CalendarFormat.twoWeeks: '2 Weeks',
+              CalendarFormat.week: 'Week',
+            },
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              titleTextStyle: TextStyle(
+                color: gray4,
+                fontFamily: 'PretandardMedium',
+                fontSize: 20.0,
+              ),
+              headerPadding: EdgeInsets.fromLTRB(0, 8.0, 0, 8.0),
+            ),
+            calendarStyle: CalendarStyle(
+              holidayTextStyle: TextStyle(
+                color: gray4,
+                fontFamily: 'PretandardMedium',
+                fontSize: 15.0,
+              ),
+              defaultTextStyle: TextStyle(
+                color: gray4,
+                fontFamily: 'PretandardMedium',
+                fontSize: 15.0,
+              ),
+              // today 표시 여부
+              isTodayHighlighted: true,
 
-          // today 글자 조정
-          todayTextStyle: const TextStyle(
-            color: const Color(0xFFFAFAFA),
-            fontSize: 16.0,
-          ),
+              // today 글자 조정
+              todayTextStyle: const TextStyle(
+                color: const Color(0xFFFAFAFA),
+                fontSize: 16.0,
+              ),
 
-          // today 모양 조정
-          todayDecoration: const BoxDecoration(
-            color: iris_80,
-            shape: BoxShape.circle,
-          ),
+              // today 모양 조정
+              todayDecoration: const BoxDecoration(
+                color: iris_80,
+                shape: BoxShape.circle,
+              ),
 
-          selectedDecoration: const BoxDecoration(
-            color: iris_100,
-            shape: BoxShape.circle,
-          ),
+              selectedDecoration: const BoxDecoration(
+                color: iris_100,
+                shape: BoxShape.circle,
+              ),
 
-          // marker 모양 조정
-          markerSizeScale: 0.1,
-          markersAnchor: 3, // 마커 위치 조정
+              // marker 모양 조정
+              markerSizeScale: 0.1,
+              markersAnchor: 3, // 마커 위치 조정
 
-          markerDecoration: const BoxDecoration(
-            color: pink,
-            shape: BoxShape.circle,
+              markerDecoration: const BoxDecoration(
+                color: pink,
+                shape: BoxShape.circle,
+              ),
+            ),
+            eventLoader: _getEventsForDay,
           ),
-        ),
-        eventLoader: _getEventsForDay,
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: _buildEventList(),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildEventList() {
+    final events = selectedDay != null ? _getEventsForDay(selectedDay!) : [];
+    return ListView(
+      children: events.map((record) {
+        return Card(
+          color: iris_60,
+          elevation: 2, // 그림자 높이
+          margin:
+              EdgeInsets.symmetric(vertical: 8, horizontal: 16), // 리스트 간격 조절
+          child: ListTile(
+            title: Text(
+              '${formatDate(record.date)}',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'PretandardMedium',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '시작 시간: ${record.startTime}',
+                  style: TextStyle(
+                    fontFamily: 'PretandardMedium',
+                    fontSize: 14.0,
+                  ),
+                ),
+                Text(
+                  '종료 시간: ${record.endTime}',
+                  style: TextStyle(
+                    fontFamily: 'PretandardMedium',
+                    fontSize: 14.0,
+                  ),
+                ),
+                Text(
+                  '페이스: ${record.pace}',
+                  style: TextStyle(
+                    fontFamily: 'PretandardMedium',
+                    fontSize: 14.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
