0df66f59f1bd68abb2343fc4c8e518b684cd1cb6
조하은 <108499120+hani0903@users.noreply.github.com>
2024-05-30 19:38:40 +0900

    Add: 블루투스 연결 버튼 추가
    블루투스 연결 화면으로 이동하는 버튼을 추가하였습니다.

--- front/running_mate/lib/screens/HomeScreen.dart
+++ front/running_mate/lib/screens/HomeScreen.dart
@@ -150,7 +150,6 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               child: TextButton(
                 onPressed: () {
-                  
                   Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -160,8 +159,31 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
                 child: Text(
                   '장거리 달리기 연습하기',
-                  style: TextStyle(
-                      color: Colors.black), 
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 25.0),
+              width: double.infinity,
+              height: 60.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0), // 버튼의 모서리를 둥글게 만듭니다.
+                border: Border.all(color: Colors.grey), // 회색 선으로 버튼 테두리를 만듭니다.
+                color: Colors.white, // 버튼의 배경색을 흰색으로 설정합니다.
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => PracticeScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  '블루투스 연결하기',
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
             )
